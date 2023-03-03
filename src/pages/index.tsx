c8658b07cc129d656d6f13527725786bdccf0ebb
ibimina <96787210+ibimina@users.noreply.github.com>
2023-03-03 14:12:04 +0100

    docs: add html template

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,46 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <form className={styles.form}>
+        <section className={styles.invoice}>
+          <div className={styles.address}>
+            <textarea name="from" cols={30} rows={10} placeholder={'Your Company or Name\nAddress'}></textarea>
+            <textarea name="billto" cols={30} rows={10} placeholder="Your customer's billing address"></textarea>
+          </div>
+          <div className={styles.invoice_info}>
+            <input type="file" name="logo" />
+            <input type="text" name='Invoice' placeholder='Invoice number' />
+            <input type='text' name='po' placeholder="Purchase Order" />
+            <input type="date" name="date" />
+          </div>
 
+        </section>
+        <section className={styles.invoice_itemlist}>
+          <section className={styles.invoice_item}>
+            <textarea name="Description" cols={30} rows={2} placeholder="Description"></textarea>
+            <input type='number' name='unit' placeholder="0.0" />
+            <input type='number' name='quantity' placeholder="Qunatity" />
+            <input type='number' name='Amount' placeholder="0.0" />
+            <button>Add tax</button>
+            <button>remove item</button>
+          </section>
+        </section>
+        <button>Add item</button>
+        <div className={styles.calwrap}>
+          <div className={styles.cal}>
+            <p>Subtotal</p>
+            <p>vat</p>
+            <p>GrandTotal</p>
+          </div>
+         
+        </div>
+
+        <button>Generate</button>
+        <section className={styles.seal}>
+          <textarea name="Terms" cols={30} rows={10} placeholder="optional"></textarea>
+          <input type="file" name="signature" />
+        </section>
+      </form>
     </>
   )
 }
