21508c400d907a26d30a86885034885385b53be6
ibimina <96787210+ibimina@users.noreply.github.com>
2023-03-05 20:47:09 +0100

    docs: form initial state

---
+++ src/input/Input.tsx
@@ -0,0 +1,28 @@
+export const Invoice = {
+    from: "",
+    billto: "",
+    logo: {
+        lastModified: 0,
+        lastModifiedDate: 0,
+        name: "",
+        size: 0,
+        type: "",
+        webkitRelativePath: ""
+    },
+    Invoice: "",
+    po: "",
+    date: "",
+    itemlist: [],
+    subtotal: 0,
+    vat: 0,
+    grandtotal: 0,
+    Terms: "",
+    signature: {
+        lastModified  :  0,
+lastModifiedDate  : 0,
+name:"",
+size:0,
+type:"",
+webkitRelativePath:""
+}
+}
