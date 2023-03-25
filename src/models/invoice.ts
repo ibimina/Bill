502f4838b963132137ce54f6205ff891ed56a31a
Ibimina hart <ibimina.c.hart@gmail.com>
2023-03-25 18:38:57 +0100

    docs: interface

--- src/models/invoice.ts
+++ src/models/invoice.ts
@@ -2,20 +2,24 @@ export interface Invoicee {
     from: string
     billto: string
     logo: {}
-    Invoice: string
+    invoicenum: string
     po: string
     date: string
     itemlist: Item[]
     subtotal: number
-    vat: number
-    grandtotal: number
+    total: number
     Terms: string
     signature: {}
+    tax?:Tax
+    vat: number
 }
 export interface Item {
     description: string
     unit: number
     quantity: number
     Amount: number
-    tax?: number
+}
+export interface Tax {
+    tax_description: string
+    taxrate: number
 }
